e445fe658309689d9d2af889f36144c87f5c68d7
Adam J. Leuer <ajleuer@gmail.com>
2016-01-04 16:43:25 -0600

    Initial Commit

---
+++ Excel Data Copy Tool/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Excel Data Copy Tool
+//
+//  Created by Adam James Leuer on 1/4/16.
+//  Copyright (c) 2016 Adam James Leuer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
